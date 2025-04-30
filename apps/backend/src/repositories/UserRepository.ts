5f7c5e7ce6e4ded4f206e72655cf4810b111780a
Filip Kovac <xkovac9@fi.muni.cz>
2025-04-30 17:26:43 +0200

    feat: implemented UserRepository

--- apps/backend/src/repositories/UserRepository.ts
+++ apps/backend/src/repositories/UserRepository.ts
@@ -1,3 +1,42 @@
 import { IUserRepository } from '@devbits/shared';
+import { prisma, Prisma, User } from 'packages/database';
 
-class UserRepository implements IUserRepository {}
+class UserRepository implements IUserRepository {
+    createUser(createUser: Prisma.UserCreateInput): Promise<User> {
+        return prisma.user.create({
+            data: createUser,
+        });
+    }
+
+    readUserById(userId: string): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: {
+                userId: userId,
+            },
+        });
+    }
+
+    readUserByUsername(username: string): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: {
+                username: username,
+            },
+        });
+    }
+
+    updateUser(
+        where: Prisma.UserWhereUniqueInput,
+        data: Prisma.UserUpdateInput
+    ): Promise<User> {
+        return prisma.user.update({
+            where,
+            data,
+        });
+    }
+
+    deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return prisma.user.delete({
+            where,
+        });
+    }
+}
