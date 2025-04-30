d179d47aed94f57d344b51586425625423454a65
Filip Kovac <xkovac9@fi.muni.cz>
2025-04-30 17:26:31 +0200

    feat: created the IUserRepository

--- packages/shared/interfaces/repositories/IUserRepository.ts
+++ packages/shared/interfaces/repositories/IUserRepository.ts
@@ -1,1 +1,19 @@
-export interface IUserRepository {}
+import { Prisma, User } from '@devbits/database';
+
+export interface IUserRepository {
+    // Create
+    createUser(createUser: Prisma.UserCreateInput): Promise<User>;
+
+    // Read
+    readUserById(userId: string): Promise<User | null>;
+    readUserByUsername(username: string): Promise<User | null>;
+
+    // Update
+    updateUser(
+        where: Prisma.UserWhereUniqueInput,
+        data: Prisma.UserUpdateInput
+    ): Promise<User>;
+
+    // Delete
+    deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User>;
+}
