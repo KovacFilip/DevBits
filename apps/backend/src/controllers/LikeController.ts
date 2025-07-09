148becddde2dd4ed25581add7bfc4e9a6f280691
Filip Kovac <xkovac9@fi.muni.cz>
2025-07-09 14:51:30 +0200

    feat: likeController logger DI

--- apps/backend/src/controllers/LikeController.ts
+++ apps/backend/src/controllers/LikeController.ts
@@ -1,4 +1,8 @@
-import { SERVICE_IDENTIFIER } from 'apps/backend/src/constants/identifiers';
+import {
+    LOGGER,
+    SERVICE_IDENTIFIER,
+} from 'apps/backend/src/constants/identifiers';
+import { logControllerErrorTrace } from 'apps/backend/src/helpers/loggingHelpers';
 import { ILikeController } from 'apps/backend/src/models/interfaces/controllers/ILikeController';
 import { ILikeService } from 'apps/backend/src/models/interfaces/services/ILikeService';
 import { FastifyReply, FastifyRequest } from 'fastify';
@@ -11,93 +15,189 @@ import {
     LikePostDTO,
     PostIdDTO,
 } from 'packages/shared';
+import { Logger } from 'pino';
 
 @injectable()
 export class LikeController implements ILikeController {
     constructor(
         @inject(SERVICE_IDENTIFIER.LIKE_SERVICE)
-        readonly likeService: ILikeService
+        readonly likeService: ILikeService,
+        @inject(LOGGER.LOGGER) readonly logger: Logger
     ) {}
 
     async likePost(
         request: FastifyRequest<{ Params: PostIdDTO }>,
         response: FastifyReply<{ Reply: LikeIdDTO }>
     ): Promise<void> {
-        const like = await this.likeService.likePost(
-            request.user,
-            request.params
-        );
+        try {
+            const like = await this.likeService.likePost(
+                request.user,
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(like);
+            return response.code(StatusCodes.OK).send(like);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'likePost',
+            });
+
+            throw err;
+        }
     }
 
     async likeComment(
         request: FastifyRequest<{ Params: CommentIdDTO }>,
         response: FastifyReply<{ Reply: LikeIdDTO }>
     ): Promise<void> {
-        const like = await this.likeService.likeComment(
-            request.user,
-            request.params
-        );
+        try {
+            const like = await this.likeService.likeComment(
+                request.user,
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(like);
+            return response.code(StatusCodes.OK).send(like);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'likeComment',
+            });
+
+            throw err;
+        }
     }
 
     async getLike(
         request: FastifyRequest<{ Params: LikeIdDTO }>,
         response: FastifyReply<{ Reply: LikePostDTO | LikeCommentDTO }>
     ): Promise<void> {
-        const like = await this.likeService.getLike(request.params);
+        try {
+            const like = await this.likeService.getLike(request.params);
 
-        return response.code(StatusCodes.OK).send(like);
+            return response.code(StatusCodes.OK).send(like);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'getLike',
+            });
+
+            throw err;
+        }
     }
 
     async getLikesForPost(
         request: FastifyRequest<{ Params: PostIdDTO }>,
         response: FastifyReply<{ Reply: LikeIdDTO[] }>
     ): Promise<void> {
-        const likes = await this.likeService.getLikesForPost(request.params);
+        try {
+            const likes = await this.likeService.getLikesForPost(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(likes);
+            return response.code(StatusCodes.OK).send(likes);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'getLikesForPost',
+            });
+
+            throw err;
+        }
     }
 
     async getLikesForComment(
         request: FastifyRequest<{ Params: CommentIdDTO }>,
         response: FastifyReply<{ Reply: LikeIdDTO[] }>
     ): Promise<void> {
-        const likes = await this.likeService.getLikesForComment(request.params);
+        try {
+            const likes = await this.likeService.getLikesForComment(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(likes);
+            return response.code(StatusCodes.OK).send(likes);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'getLikesForComment',
+            });
+
+            throw err;
+        }
     }
 
     async getPostLikesCount(
         request: FastifyRequest<{ Params: PostIdDTO }>,
         response: FastifyReply<{ Reply: number }>
     ): Promise<void> {
-        const count = await this.likeService.getNumberOfLikesOfPost(
-            request.params
-        );
+        try {
+            const count = await this.likeService.getNumberOfLikesOfPost(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(count);
+            return response.code(StatusCodes.OK).send(count);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'getPostLikesCount',
+            });
+
+            throw err;
+        }
     }
 
     async getCommentLikesCount(
         request: FastifyRequest<{ Params: CommentIdDTO }>,
         response: FastifyReply<{ Reply: number }>
     ): Promise<void> {
-        const count = await this.likeService.getNumberOfLikesOfComment(
-            request.params
-        );
+        try {
+            const count = await this.likeService.getNumberOfLikesOfComment(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(count);
+            return response.code(StatusCodes.OK).send(count);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'getCommentLikesCount',
+            });
+
+            throw err;
+        }
     }
 
     async deleteLike(
         request: FastifyRequest<{ Params: LikeIdDTO }>,
         response: FastifyReply<{ Reply: LikeIdDTO }>
     ): Promise<void> {
-        const deletedLike = await this.likeService.removeLike(request.params);
+        try {
+            const deletedLike = await this.likeService.removeLike(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(deletedLike);
+            return response.code(StatusCodes.OK).send(deletedLike);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'LikeController',
+                method: 'deleteLike',
+            });
+
+            throw err;
+        }
     }
 }
