732608573ffd0d46bba521cf6916fa7ee444edce
Filip Kovac <xkovac9@fi.muni.cz>
2025-07-09 14:57:50 +0200

    feat: commentController logger DI

--- apps/backend/src/controllers/CommentController.ts
+++ apps/backend/src/controllers/CommentController.ts
@@ -1,4 +1,8 @@
-import { SERVICE_IDENTIFIER } from 'apps/backend/src/constants/identifiers';
+import {
+    LOGGER,
+    SERVICE_IDENTIFIER,
+} from 'apps/backend/src/constants/identifiers';
+import { logControllerErrorTrace } from 'apps/backend/src/helpers/loggingHelpers';
 import { ICommentController } from 'apps/backend/src/models/interfaces/controllers/ICommentController';
 import { ICommentService } from 'apps/backend/src/models/interfaces/services/ICommentService';
 import { FastifyReply, FastifyRequest } from 'fastify';
@@ -13,64 +17,110 @@ import {
     UpdateCommentDTO,
     UserIdDTO,
 } from 'packages/shared';
+import { Logger } from 'pino';
 
 @injectable()
 export class CommentController implements ICommentController {
     constructor(
         @inject(SERVICE_IDENTIFIER.COMMENT_SERVICE)
-        readonly commentService: ICommentService
+        readonly commentService: ICommentService,
+        @inject(LOGGER.LOGGER) readonly logger: Logger
     ) {}
 
     async createComment(
         request: FastifyRequest<{ Body: CreateCommentDTO }>,
         response: FastifyReply<{ Reply: CommentDTO }>
     ): Promise<void> {
-        const user = request.user;
-        const newComment = await this.commentService.createComment(
-            { userId: user.userId },
-            {
-                ...request.body,
-            }
-        );
+        try {
+            const user = request.user;
+            const newComment = await this.commentService.createComment(
+                { userId: user.userId },
+                {
+                    ...request.body,
+                }
+            );
 
-        return response.code(StatusCodes.OK).send(newComment);
+            return response.code(StatusCodes.OK).send(newComment);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'createComment',
+            });
+
+            throw err;
+        }
     }
 
     async getComment(
         request: FastifyRequest<{ Params: CommentIdDTO }>,
         response: FastifyReply<{ Reply: CommentDTO }>
     ): Promise<void> {
-        const { commentId } = request.params;
+        try {
+            const { commentId } = request.params;
 
-        const comment = await this.commentService.getComment({ commentId });
+            const comment = await this.commentService.getComment({ commentId });
 
-        return response.code(StatusCodes.OK).send(comment);
+            return response.code(StatusCodes.OK).send(comment);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'getComment',
+            });
+
+            throw err;
+        }
     }
 
     async getCommentsForPost(
         request: FastifyRequest<{ Params: PostIdDTO }>,
         response: FastifyReply<{ Reply: SimpleCommentDTO[] }>
     ): Promise<void> {
-        const { postId } = request.params;
+        try {
+            const { postId } = request.params;
 
-        const comments = await this.commentService.getCommentsForPost({
-            postId,
-        });
+            const comments = await this.commentService.getCommentsForPost({
+                postId,
+            });
 
-        return response.code(StatusCodes.OK).send(comments);
+            return response.code(StatusCodes.OK).send(comments);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'getCommentsForPost',
+            });
+
+            throw err;
+        }
     }
 
     async getCommentsByUser(
         request: FastifyRequest<{ Params: UserIdDTO }>,
         response: FastifyReply<{ Reply: SimpleCommentDTO[] }>
     ): Promise<void> {
-        const { userId } = request.params;
+        try {
+            const { userId } = request.params;
 
-        const comments = await this.commentService.getCommentsByUser({
-            userId,
-        });
+            const comments = await this.commentService.getCommentsByUser({
+                userId,
+            });
 
-        return response.code(StatusCodes.OK).send(comments);
+            return response.code(StatusCodes.OK).send(comments);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'getCommentsByUser',
+            });
+
+            throw err;
+        }
     }
 
     async updateComment(
@@ -80,22 +130,44 @@ export class CommentController implements ICommentController {
         }>,
         response: FastifyReply<{ Reply: CommentDTO }>
     ): Promise<void> {
-        const updatedComment = await this.commentService.updateComment(
-            request.params,
-            request.body
-        );
+        try {
+            const updatedComment = await this.commentService.updateComment(
+                request.params,
+                request.body
+            );
 
-        return response.code(StatusCodes.OK).send(updatedComment);
+            return response.code(StatusCodes.OK).send(updatedComment);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'updateComment',
+            });
+
+            throw err;
+        }
     }
 
     async deleteComment(
         request: FastifyRequest<{ Params: CommentIdDTO }>,
         response: FastifyReply<{ Reply: SimpleCommentDTO }>
     ): Promise<void> {
-        const deletedComment = await this.commentService.deleteComment(
-            request.params
-        );
+        try {
+            const deletedComment = await this.commentService.deleteComment(
+                request.params
+            );
 
-        return response.code(StatusCodes.OK).send(deletedComment);
+            return response.code(StatusCodes.OK).send(deletedComment);
+        } catch (err) {
+            logControllerErrorTrace({
+                logger: this.logger,
+                request,
+                controller: 'CommentController',
+                method: 'deleteComment',
+            });
+
+            throw err;
+        }
     }
 }
