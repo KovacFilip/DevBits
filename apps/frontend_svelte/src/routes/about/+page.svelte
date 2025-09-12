25ac1cebb6baca552fc52ae30d258dcc33d2d0ac
Filip Kovac <kovacfilipmh@gmail.com>
2025-09-12 20:09:46 +0200

    chore: added navigation menu shadcn

---
+++ apps/frontend_svelte/src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	let s = $state(false);
+</script>
+
+<div class="flex flex-col bg-red-500">
+	<div>This is my about page</div>
+	<Button
+		onclick={() => {
+			s = !s;
+		}}>Click me</Button
+	>
+	{#if s}
+		<div>s is true</div>
+	{/if}
+</div>
